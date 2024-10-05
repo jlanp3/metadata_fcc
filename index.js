7474f950c515af7be6d383c37374162bcec6ae95
jlanp3 <jlanp3@gmail.com>
2024-10-05 18:18:04 -0300

    feat: added multer package

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/uploads", express.static(process.cwd() + "/uploads"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.any(), function (req, res) {
+  if (req.files.length < 1) {
+    res.json({ error: "no file added" });
+  } else {
+    const { originalname, mimetype, size } = req.files[0];
+    res.json({ name: originalname, type: mimetype, size });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
